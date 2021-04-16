9964459239da4aebea87114e242d783099c873e7
J. Bromley <jay@appliedinvention.com>
2021-04-15 23:27:26 -0700

    Initial attempt at image weights.
    Implemented ramps in the middle half of the image. Doesn't work -
    crashes directly into a ducky.

--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -3,14 +3,25 @@ from typing import Tuple
 import numpy as np
 
 
-def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
+def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
+    h, w = shape
+    left_knee = w // 4
+    right_knee = 3 * w // 4
+    res[:, 0:left_knee] = 1
+    for x in range(left_knee, right_knee):
+        res[:, x] = 1 - 2 * (x - left_knee) / (right_knee - left_knee)
+    res[:, right_knee:w] = -1
     return res
 
 
-def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
+def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
-    res[100:150, 100:300] = -1
+    h, w = shape
+    left_knee = w // 4
+    right_knee = 3 * w // 4
+    res[:, 0:left_knee] = -1
+    for x in range(left_knee, right_knee):
+        res[:, x] = -1 + 2 * (x - left_knee) / (right_knee - left_knee)
+    res[:, right_knee:w] = 1
     return res
