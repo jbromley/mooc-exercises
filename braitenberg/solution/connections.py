9f0c33a2bbf0ac9ee87148a37d43c8c9c52ed29a
J. Bromley <jay@appliedinvention.com>
2021-04-23 17:29:04 -0700

    Added ramps to the activation matrices' sides.
    Make turning away proportional to how close to the edge the obstruction
    is.

--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -6,10 +6,14 @@ import numpy as np
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     h, w = shape
+    kneept = w // 4
+    knee_width = w // 4
     offpt = 3 * w // 8
     farpt = h // 2
 
-    res[farpt:, :offpt] = 1
+    for x in range(0, kneept):
+        res[farpt:, x] = x / knee_width
+    res[farpt:, :offpt] = 1.0
     return res
 
 
@@ -17,8 +21,12 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     h, w = shape
     onpt = 3 * w // 8
+    knee_width = w // 4
+    kneept = w - knee_width
     farpt = h // 2
 
-    res[farpt:, onpt:] = 1
+    res[farpt:, onpt:kneept] = 1.0
+    for x in range(kneept, w):
+        res[farpt:, x] = 1.0 - (x - kneept) / knee_width
 
     return res
