d56e63c1bae6d4ae251b02fb8fc6930971862060
J. Bromley <jay@appliedinvention.com>
2021-04-26 23:27:46 -0700

    Reverted to simple response matrix.
    Made submission.

--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -6,14 +6,12 @@ import numpy as np
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     h, w = shape
-    kneept = w // 4
-    knee_width = w // 4
+    onpt = 0
     offpt = 3 * w // 8
     farpt = h // 2
 
-    for x in range(0, kneept):
-        res[farpt:, x] = x / knee_width
-    res[farpt:, :offpt] = 1.0
+    res[farpt:, onpt:offpt] = 1.0
+
     return res
 
 
@@ -21,12 +19,9 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     h, w = shape
     onpt = 3 * w // 8
-    knee_width = w // 4
-    kneept = w - knee_width
+    offpt = w
     farpt = h // 2
 
-    res[farpt:, onpt:kneept] = 1.0
-    for x in range(kneept, w):
-        res[farpt:, x] = 1.0 - (x - kneept) / knee_width
+    res[farpt:, onpt:offpt] = 1.0
 
     return res
