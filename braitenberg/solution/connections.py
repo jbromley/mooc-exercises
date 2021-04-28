4eb07e9d31f83eb1e08c185d7320d4886bca0513
J. Bromley <jay@appliedinvention.com>
2021-04-28 11:46:13 -0700

    Achieved 4.95 m.
    Set gain = 0.2, const = 0.4. Block response with 1/4 margin.

--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -3,11 +3,11 @@ from typing import Tuple
 import numpy as np
 
 
-def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
+def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     h, w = shape
-    onpt = 0
-    offpt = 3 * w // 8
+    onpt = 7 * w // 16
+    offpt = 3 * w // 4
     farpt = h // 2
 
     res[farpt:, onpt:offpt] = 1.0
@@ -15,11 +15,11 @@ def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     return res
 
 
-def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
+def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")
     h, w = shape
-    onpt = 3 * w // 8
-    offpt = w
+    onpt = w // 4
+    offpt = 7 * w // 16
     farpt = h // 2
 
     res[farpt:, onpt:offpt] = 1.0
